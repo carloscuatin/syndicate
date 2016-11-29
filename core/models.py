6c947374c406b4f2c89cd541674a7b7589973a05
Carlos Cuatin <cecg1996@gmail.com>
2016-11-29 16:02:27 -0500

    fix in models

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     type = models.CharField(max_length=30, choices=(('advance', 'Advance'),))
     date = models.DateField(verbose_name='Date')
     amount = models.DecimalField(max_digits=10, decimal_places=3)
-    amount_discount = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    amount_discount = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
 
 
 class Investor(models.Model):
@@ -17,7 +17,6 @@ class Purchase(models.Model):
     investor = models.ForeignKey(Investor)
     product = models.ForeignKey(Product)
     amount_to_sell = models.DecimalField(max_digits=10, decimal_places=3)
-    left_amount = models.DecimalField(max_digits=10, decimal_places=3)
 
     @property
     def percentage(self):
