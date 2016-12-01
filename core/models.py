b81c4e8a612c69a86078c3127cfa65eafc6affc6
Carlos Cuatin <cecg1996@gmail.com>
2016-12-01 11:11:57 -0500

    signals

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class Product(models.Model):
     product_id = models.CharField(max_length=30)
     type = models.CharField(max_length=30, choices=(('advance', 'Advance'),))
     date = models.DateField(verbose_name='Date')
-    amount = models.DecimalField(max_digits=10, decimal_places=3)
-    amount_discount = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=0)
+    amount_discount = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
 
 
 class Investor(models.Model):
@@ -16,7 +16,7 @@ class Investor(models.Model):
 class Purchase(models.Model):
     investor = models.ForeignKey(Investor)
     product = models.ForeignKey(Product)
-    amount_to_sell = models.DecimalField(max_digits=10, decimal_places=3)
+    amount_to_sell = models.DecimalField(max_digits=10, decimal_places=0)
 
     @property
     def percentage(self):
--- core/signals.py
+++ core/signals.py
@@ -1,4 +1,5 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
+
 from core.models import Purchase, Product
 
 
@@ -8,16 +9,25 @@ def purchase_post_save(sender, instance=None, created=False, **kwargs):
         product.amount_discount -= instance.amount_to_sell
         product.save()
 
-def product_post_save(sender, instance=None, created=False, **kwargs):
-    if created:
-        instance.amount_discount = instance.amount
-        instance.save()
 
 def purchase_post_delete(sender, instance=None, created=False, **kwargs):
     product = instance.product
     product.amount_discount += instance.amount_to_sell
     product.save()
 
-post_save.connect(purchase_post_save, sender=Purchase)
+
+def purchase_pre_save(sender, instance=None, **kwargs):
+    if instance.amount_to_sell > instance.product.amount_discount:
+        raise Exception("amount_to_sell can't be higher to amount_discount")
+
+
+def product_post_save(sender, instance=None, created=False, **kwargs):
+    if created:
+        instance.amount_discount = instance.amount
+        instance.save()
+
+
 post_save.connect(product_post_save, sender=Product)
+post_save.connect(purchase_post_save, sender=Purchase)
+pre_save.connect(purchase_pre_save, sender=Purchase)
 post_delete.connect(purchase_post_delete, sender=Purchase)
