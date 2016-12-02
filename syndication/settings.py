5650f816f17b13ae088ebb3c84f7949c564f0b25
Carlos Cuatin <cecg1996@gmail.com>
2016-12-02 13:01:04 -0500

    fixs

--- syndication/settings.py
+++ syndication/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'syndication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dc5ege9d3fr3qc',
+        'USER': 'trjdktpruzvaon',
+        'PASSWORD': 'wdM0FGDBoPt2B1PZiM_h6BDqJ-',
+        'HOST': 'ec2-184-73-222-194.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
